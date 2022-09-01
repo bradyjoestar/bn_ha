747674c7372d35cf7e8b6446457a510f58b8461e
wenbin <wenbin.shi@gmail.com>
2022-09-01 21:43:17 +0800

    fix bug

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 git clone https://github.com/bitnetworkio/bitnetwork
 
 cp -r haenvs bitnetwork/ops/
-cp -r docker-compose.az0.yml docker-compose.az1.yml docker-compose.ec2.yml bitnetwork/ops/
+cp -r docker-compose.az0.yml  bitnetwork/ops/
+cp -r docker-compose.az1.yml  bitnetwork/ops/
+cp -r docker-compose.ec2.yml  bitnetwork/ops/
 
 cd bitnetwork/ops/
 
-docker-compose -f docker-compose.az0.yml -f docker-compose.az1.yml -f  docker-compose.ec2.yml up
+docker-compose -f docker-compose.az0.yml -f docker-compose.az1.yml -f docker-compose.ec2.yml up
