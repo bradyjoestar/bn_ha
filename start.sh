78cc351950e0ca7dc41e637110f37dbd98f431e5
wenbin <wenbin.shi@gmail.com>
2022-09-08 14:39:10 +0800

    update bn_ha

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/bitnetworkio/bitnetwork
+git clone https://github.com/mantlenetworkio/mantle
 
-cp -r haenvs bitnetwork/ops/
-cp -r docker-compose.az0.yml  bitnetwork/ops/
-cp -r docker-compose.az1.yml  bitnetwork/ops/
-cp -r docker-compose.ec2.yml  bitnetwork/ops/
-cp -r docker-compose-master-slave-change.yml up bitnetwork/ops/
-cp -r master_slave_change.sh bitnetwork/ops/
+cp -r haenvs mantle/ops/
+cp -r docker-compose.az0.yml  mantle/ops/
+cp -r docker-compose.az1.yml  mantle/ops/
+cp -r docker-compose.ec2.yml  mantle/ops/
+cp -r docker-compose-master-slave-change.yml  mantle/ops/
+cp -r master_slave_change.sh mantle/ops/
 
-cd bitnetwork/ops/
+cd mantle/ops/
 
 docker-compose -f docker-compose.az0.yml -f docker-compose.az1.yml -f docker-compose.ec2.yml up
